d703568cbc54dca48f26865fa55f7efc269fed1e
Roy Sun <syzroy@gmail.com>
2025-03-26 16:37:32 +0000

    update to use opts as per the documentation recommends

--- nvim/lua/plugins/js.lua
+++ nvim/lua/plugins/js.lua
@@ -1,29 +1,18 @@
 return {
-  {
-    "vuki656/package-info.nvim",
-    ft = "json",
-    dependencies = "MunifTanjim/nui.nvim",
-    keys = {
-      {
-        "<leader>ns",
-        function()
-          require("package-info").show({ force = true })
-        end,
-        desc = "Force Refresh Package Info",
-      },
+  "vuki656/package-info.nvim",
+  ft = "json",
+  dependencies = "MunifTanjim/nui.nvim",
+  keys = {
+    {
+      "<leader>ns",
+      function()
+        require("package-info").show({ force = true })
+      end,
+      desc = "Force Refresh Package Info",
     },
-    opts = {
-      colors = {
-        up_to_date = "#98c379", -- Text color for up to date dependency virtual text
-        outdated = "#d19a66", -- Text color for outdated dependency virtual text
-      },
-    },
-    config = function(_, opts)
-      require("package-info").setup(opts)
-
-      -- manually register them
-      vim.cmd([[highlight PackageInfoUpToDateVersion guifg=]] .. opts.colors.up_to_date)
-      vim.cmd([[highlight PackageInfoOutdatedVersion guifg=]] .. opts.colors.outdated)
-    end,
   },
+  opts = function()
+    vim.cmd([[highlight PackageInfoUpToDateVersion guifg=]] .. "#98c379")
+    vim.cmd([[highlight PackageInfoOutdatedVersion guifg=]] .. "#d19a66")
+  end,
 }
--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,9 +1,9 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin-frappe",
-    },
-  },
+  "catppuccin/nvim",
+  lazy = false,
+  name = "catppuccin",
+  priority = 1000,
+  opts = function()
+    vim.cmd.colorscheme("catppuccin-frappe")
+  end,
 }
