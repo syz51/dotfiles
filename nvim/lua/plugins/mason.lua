418dba2bb58791459cc70921357fdca08172ce1a
Roy Sun <syzroy@gmail.com>
2026-07-01 14:53:41 +0800

    reset nvim

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,11 +1,6 @@
 return {
   "mason-org/mason.nvim",
   opts = function(_, opts)
-    vim.list_extend(opts.ensure_installed, {
-      "templ",
-      "graphql-language-service-cli",
-      "powershell-editor-services",
-      "pbls",
-    })
+    vim.list_extend(opts.ensure_installed, {})
   end,
 }
